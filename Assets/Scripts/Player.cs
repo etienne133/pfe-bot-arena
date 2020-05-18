1cefdcbd41bcbee0b88129b5f8132f4495f325e7
Guillaume <guillaume_leblanc_89@hotmail.com>
2020-05-18 19:02:38 -0400

    Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,16 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField]
+    int speed = 10;
     PlayerControls playerControls;
+    Vector2 move;
     // Start is called before the first frame update
     private void Awake()
     {
         playerControls = new PlayerControls();
         playerControls.Gameplay.Fire.performed += ctx => Fire();
+        playerControls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        playerControls.Gameplay.Move.canceled += ctx => move = Vector2.zero;
     }
 
     public void Fire()
@@ -28,5 +32,12 @@ public class Player : MonoBehaviour
     private void OnDisable()
     {
         playerControls.Gameplay.Disable();
+    }
+
+    private void Update()
+    {
+        // vertical axis is Y so move on X and Z.
+        Vector3 moveVector = new Vector3(move.x, 0, move.y) * Time.deltaTime * speed;
+        transform.Translate(moveVector, Space.Self);
     }
 }
