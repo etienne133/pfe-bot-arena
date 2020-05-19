465878e12cee086fe7c193f022cf881f2fc5b8fe
Guillaume <guillaume_leblanc_89@hotmail.com>
2020-05-18 20:43:26 -0400

    rotationstillbroken

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,22 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    int speed = 10;
+    int moveSpeed = 10, turnSpeed = 10;
     PlayerControls playerControls;
     Vector2 move;
+    Vector2 aim;
     // Start is called before the first frame update
     private void Awake()
     {
         playerControls = new PlayerControls();
         playerControls.Gameplay.Fire.performed += ctx => Fire();
+
         playerControls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
         playerControls.Gameplay.Move.canceled += ctx => move = Vector2.zero;
+
+        playerControls.Gameplay.Aim.performed += ctx => aim = ctx.ReadValue<Vector2>();
+        playerControls.Gameplay.Aim.canceled += ctx => aim = Vector2.zero;
+
     }
 
     public void Fire()
@@ -36,8 +42,19 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        // move
         // vertical axis is Y so move on X and Z.
-        Vector3 moveVector = new Vector3(move.x, 0, move.y) * Time.deltaTime * speed;
+        Vector3 moveVector = new Vector3(move.x, 0, move.y) * Time.deltaTime * moveSpeed;
         transform.Translate(moveVector, Space.Self);
+
+        // aim
+        //Vector3 mPos = new Vector3(aim.x, 0 , aim.y);
+        //transform.Rotate(r, Space.Self);
+
+        //Vector2 lookDir = mPos - transform.position;
+        //float angle = Mathf.Atan2(lookDir.y,lookDir.x) * Mathf.Rad2Deg - 90f;
+        //Quaternion rotationQ = new Quaternion();
+        //rotationQ.SetFromToRotation(transform.position, mPos);
+        //transform.rotation = rotationQ * transform.rotation;
     }
 }
