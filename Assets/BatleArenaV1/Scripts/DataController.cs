3848cdc3049a4b4efefa8a9ace8520a17b11b948
Guillaume Leblanc <guillaume_leblanc_89@hotmail.com>
2020-07-29 14:36:09 -0400

    Debut DataController

---
+++ Assets/BatleArenaV1/Scripts/DataController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Handles data serialisation/compute etc
+public class DataController : MonoBehaviour
+{
+    class PlayerData
+    {
+        public int playerID { get; set; }
+        public int Kills { get; set; }
+        public int Deaths { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int shots { get; set; }//optional
+        public int accuracy { get; set; }//optional
+
+
+    }
+
+    private Dictionary<int, PlayerData> playerdictionary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerdictionary = new Dictionary<int, PlayerData>();
+    }
+
+    public void initializePlayers(List<int> playerIDs)
+    {
+        playerIDs.ForEach(id => {
+            playerdictionary.Add(id, new PlayerData());
+        }) ;
+    }
+    public void registerKill(int killerId, int victimID)
+    {
+        playerdictionary[killerId].Kills++;
+        playerdictionary[victimID].Deaths++;
+    }
+
+}
